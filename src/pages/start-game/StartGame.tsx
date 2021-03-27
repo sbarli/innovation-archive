9fd5ba7a7937ce37ccf855bf28ecfe6d89cb9205
Samantha Salley <samanthasalley@gmail.com>
2021-03-27 03:04:45 -0400

    create start form and add to start game page

--- src/pages/start-game/StartGame.tsx
+++ src/pages/start-game/StartGame.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { StartForm } from '../../components/start-form';
+
+const onSubmit = (values: any) => {
+  console.log('StartGame >>> form submitted -> values: ', values);
+};
+
 export function StartGame() {
   return (
     <div className="StartGame">
       <h1>Welcome to the Start Game Page!</h1>
       <Link to="/">Go Home</Link>
+      <StartForm onSubmit={onSubmit} />
     </div>
   );
 }
