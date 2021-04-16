81977d3b6d3692108948df9837d76a8665e8b2a3
Samantha Salley <samanthasalley@gmail.com>
2021-04-15 21:32:29 -0400

    update to have submit call setupGame redux action

--- src/pages/start-game/StartGame.tsx
+++ src/pages/start-game/StartGame.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { setupGame } from '../../actions/gameActions';
 import { StartForm } from '../../components/start-form';
 
-const onSubmit = (values: any) => {
-  console.log('StartGame >>> form submitted -> values: ', values);
-};
-
 export function StartGame() {
+  const dispatch = useDispatch();
+
+  const onSubmit = useCallback(
+    (values: any) => {
+      dispatch(setupGame(values));
+    },
+    [dispatch]
+  );
+
   return (
     <div className="StartGame">
       <h1>Welcome to the Start Game Page!</h1>
