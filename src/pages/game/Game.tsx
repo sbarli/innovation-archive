aec990e735a00208a8ec4ca28acc73007142315e
Samantha Salley <samanthasalley@gmail.com>
2021-04-23 11:38:04 -0400

    add CurrentPlayerView component to Game page

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { CurrentPlayerView } from '../../components/current-player-view';
 import { PlayersBar } from '../../components/players-bar';
 
 export function Game() {
@@ -7,6 +8,7 @@ export function Game() {
     <div className="Game">
       <h1>Welcome to the Game Page!</h1>
       <PlayersBar />
+      <CurrentPlayerView />
     </div>
   );
 }
