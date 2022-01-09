4f2a117d421046d2649be48d2e655a2e592e8642
Samantha Salley <samanthasalley@gmail.com>
2022-01-09 15:45:46 -0500

    create initPlayerOrder reducer

--- src/state/playersSlice.ts
+++ src/state/playersSlice.ts
@@ -19,18 +19,29 @@ export const playersSlice = createSlice({
   name: 'players',
   initialState,
   reducers: {
-    initPlayers: (
-      state,
-      {
-        payload: { players, playerOrder },
-      }: PayloadAction<{ players: IPlayers; playerOrder: string[] }>
-    ) => {
+    initPlayers: (state, { payload: { players } }: PayloadAction<{ players: IPlayers }>) => {
       // add all players
       state.players = players;
+    },
+    initPlayerOrder: (
+      state,
+      { payload: { playerOrder } }: PayloadAction<{ playerOrder: string[] }>
+    ) => {
       // add player order
       state.playerOrder = playerOrder;
       // setup current player as first player id
       state.currentPlayer = playerOrder[0];
+      // set left, right, and isFirst values for each player
+      playerOrder.forEach((playerId, idx) => {
+        if (idx === 0) {
+          // set players obj to include isFirst boolean
+          state.players[playerOrder[0]].isFirst = true;
+        }
+        state.players[playerOrder[0]].right =
+          idx === playerOrder.length - 1 ? playerOrder[0] : playerOrder[idx + 1];
+        state.players[playerOrder[0]].left =
+          idx === 0 ? playerOrder[playerOrder.length - 1] : playerOrder[idx - 1];
+      });
     },
     nextPlayer: state => {
       if (!state.currentPlayer) {
@@ -41,7 +52,7 @@ export const playersSlice = createSlice({
   },
 });
 
-export const { initPlayers, nextPlayer } = playersSlice.actions;
+export const { initPlayers, initPlayerOrder, nextPlayer } = playersSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
