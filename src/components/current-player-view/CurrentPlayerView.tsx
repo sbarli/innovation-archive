7cb3884cb104f54740d64e9c61298aadcd4d402e
Samantha Salley <samanthasalley@gmail.com>
2022-01-10 15:24:07 -0500

    add board to current player view

--- src/components/current-player-view/CurrentPlayerView.tsx
+++ src/components/current-player-view/CurrentPlayerView.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import { selectCurrentPlayer } from '../../state/playersSlice';
+import { Board } from '../board';
 import { Hand } from '../hand';
 
 export function CurrentPlayerView() {
@@ -13,7 +14,9 @@ export function CurrentPlayerView() {
 
   return (
     <div data-testid="current-player-view">
+      <h2>{currentPlayer}'s Turn</h2>
       <Hand player={currentPlayer} />
+      <Board player={currentPlayer} />
     </div>
   );
 }
