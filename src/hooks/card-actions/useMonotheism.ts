73162aca970ded69259956ed21300bd7e821b81e
Samantha Salley <samanthasalley@gmail.com>
2022-01-14 21:46:48 -0500

    create useMonotheism hook

---
+++ src/hooks/card-actions/useMonotheism.ts
@@ -0,0 +1,20 @@
+import { useCallback } from 'react';
+
+import { Ages } from '../../enums';
+import { useDrawCard } from '../dogma-actions/use-draw-card';
+import { useTuckCard } from '../dogma-actions/use-tuck-card';
+
+// Draw and tuck a 1.
+export const useMonotheism = (playerId: string) => {
+  const drawAction = useDrawCard(playerId);
+  const tuckAction = useTuckCard(playerId);
+
+  const dogma = useCallback(() => {
+    const drawnCards = drawAction({ ageToDraw: Ages.ONE, addCardToPlayerHand: false });
+    if (drawnCards) {
+      tuckAction(drawnCards[0].id);
+    }
+  }, [drawAction, tuckAction]);
+
+  return dogma;
+};
