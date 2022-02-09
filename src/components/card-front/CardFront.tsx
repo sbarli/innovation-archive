801e97ff3633be3691f7fdebf26eadd1dd4456e0
Samantha Salley <samanthasalley@gmail.com>
2022-02-08 21:05:15 -0500

    update card front with new resource styles & add card details modal logic

--- src/components/card-front/CardFront.tsx
+++ src/components/card-front/CardFront.tsx
@@ -1,35 +1,51 @@
 import React from 'react';
 
 import { CardIds } from '../../enums';
+import { useModal } from '../../hooks/use-modal';
 import { getCardById } from '../../utils/cards';
 import noop from '../../utils/noop';
 import { CardAge } from '../card-age';
+import { CardDetails } from '../card-details';
+import { Modal } from '../modal';
+import { ResourceSpace } from '../resource-space';
+import { ResourceImageSizes } from '../resource-space/ResourceSpace';
 
 import { BottomResourceRow, CardFrontWrapper, CardName, TopCardRow } from './CardFront.styled';
-import { ResourceSpace } from './resource-space';
 
 interface ICardFrontProps {
   cardId: CardIds;
   onCardClick?(): void;
 }
 export function CardFront({ cardId, onCardClick = noop }: ICardFrontProps) {
+  const { isShown, toggle } = useModal();
+
   const cardDetails = getCardById(cardId);
   if (!cardDetails) {
     return null;
   }
 
   return (
-    <CardFrontWrapper data-testid="card-front" onClick={onCardClick}>
-      <TopCardRow>
-        <ResourceSpace resource={cardDetails.resourceSpace1} />
-        <CardName $color={cardDetails.color}>{cardDetails.name}</CardName>
-        <CardAge age={cardDetails.age} />
-      </TopCardRow>
-      <BottomResourceRow>
-        <ResourceSpace resource={cardDetails.resourceSpace2} />
-        <ResourceSpace resource={cardDetails.resourceSpace3} />
-        <ResourceSpace resource={cardDetails.resourceSpace4} />
-      </BottomResourceRow>
-    </CardFrontWrapper>
+    <>
+      <CardFrontWrapper data-testid="card-front" onClick={onCardClick}>
+        <TopCardRow>
+          <ResourceSpace resource={cardDetails.resourceSpace1} size={ResourceImageSizes.SM} />
+          <CardName onClick={toggle} $color={cardDetails.color}>
+            {cardDetails.name}
+          </CardName>
+          <CardAge age={cardDetails.age} />
+        </TopCardRow>
+        <BottomResourceRow>
+          <ResourceSpace resource={cardDetails.resourceSpace2} size={ResourceImageSizes.SM} />
+          <ResourceSpace resource={cardDetails.resourceSpace3} size={ResourceImageSizes.SM} />
+          <ResourceSpace resource={cardDetails.resourceSpace4} size={ResourceImageSizes.SM} />
+        </BottomResourceRow>
+      </CardFrontWrapper>
+      <Modal
+        isShown={isShown}
+        hide={toggle}
+        headerText={`${cardDetails.name} Details`}
+        modalContent={<CardDetails cardId={cardId} />}
+      />
+    </>
   );
 }
