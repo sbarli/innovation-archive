16cd3bf31a7f75863b90d7a3a1803adca8a42140
Samantha Salley <samanthasalley@gmail.com>
2022-02-08 21:05:58 -0500

    update shared card styles

--- src/styles/card/index.ts
+++ src/styles/card/index.ts
@@ -1,6 +1,9 @@
 import { css } from 'styled-components/macro';
 
 export const cardWrapperStyles = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   background-color: var(--card-secondary);
   border: 4px solid var(--card-border);
   border-radius: 20px;
