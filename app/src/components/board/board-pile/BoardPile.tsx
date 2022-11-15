333ff11395faf937090a1ae64a95bd6d2f93f35f
Samantha Barli <samanthasalley@gmail.com>
2022-11-15 16:31:04 -0500

    add logic to kickoff dogma actino

--- app/src/components/board/board-pile/BoardPile.tsx
+++ app/src/components/board/board-pile/BoardPile.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { CardIds, Colors, SplayDirections } from '../../../enums';
+import { dogmaStart } from '../../../state/slices/dogmaSlice';
 import { CardFront } from '../../card-front';
 
 import { BoardPileWrapper, CardPosition } from './BoardPile.styled';
@@ -8,10 +10,19 @@ import { BoardPileWrapper, CardPosition } from './BoardPile.styled';
 interface IBoardPileProps {
   cardIds: CardIds[];
   color: Colors;
+  playerId: string;
   splayDirection: SplayDirections | null;
 }
 
-export const BoardPile = ({ cardIds, color, splayDirection }: IBoardPileProps) => {
+export const BoardPile = ({ cardIds, color, playerId, splayDirection }: IBoardPileProps) => {
+  const dispatch = useDispatch();
+  const dogma = useCallback(
+    (cardId: CardIds) => {
+      dispatch(dogmaStart({ dogmaCardId: cardId, playerId }));
+    },
+    [dispatch, playerId]
+  );
+
   if (!cardIds.length) {
     return <BoardPileWrapper cardsInPile={false} color={color}></BoardPileWrapper>;
   }
@@ -22,12 +33,22 @@ export const BoardPile = ({ cardIds, color, splayDirection }: IBoardPileProps) =
   const CardView = splayDirection ? (
     cardIds.map((cardId, idx) => (
       <CardPosition key={cardId} placeInPile={idx} splayDirection={splayDirection}>
-        <CardFront cardId={cardId} />
+        <CardFront
+          cardId={cardId}
+          onCardClick={
+            cardId === cardIds[cardIds.length - 1]
+              ? () => dogma(cardIds[cardIds.length - 1])
+              : undefined
+          }
+        />
       </CardPosition>
     ))
   ) : (
     <CardPosition placeInPile={cardIds.length - 1} splayDirection={splayDirection}>
-      <CardFront cardId={cardIds[cardIds.length - 1]} />
+      <CardFront
+        cardId={cardIds[cardIds.length - 1]}
+        onCardClick={() => dogma(cardIds[cardIds.length - 1])}
+      />
     </CardPosition>
   );
 
--- app/src/pages/game/Game.tsx
+++ app/src/pages/game/Game.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 
 import { Achievements } from '../../components/achievements';
 import { Deck } from '../../components/deck';
+import { Dogma } from '../../components/dogma';
 import { PlayerTabs } from '../../components/player-tabs';
 import { useAppSelector } from '../../hooks/use-app-selector';
 import { usePlayerName } from '../../hooks/use-player-name';
@@ -14,13 +15,15 @@ import { selectCurrentPlayerId, selectPlayer, selectWinner } from '../../state/s
 const GameBoardWrapper = styled.div`
   margin-top: 1rem;
   display: grid;
-  grid-template-columns: 1fr 3fr;
+  grid-template-columns: 1fr 3fr 1fr;
   justify-content: center;
 `;
 
 const PlayersWrapper = styled.div``;
 
 const CardsWrapper = styled.div``;
+
+const DogmaWrapper = styled.div``;
 
 export function Game() {
   const winnerId = useAppSelector(selectWinner);
@@ -53,6 +56,9 @@ export function Game() {
           <HeaderThree>Player Details</HeaderThree>
           <PlayerTabs />
         </PlayersWrapper>
+        <DogmaWrapper>
+          <Dogma />
+        </DogmaWrapper>
       </GameBoardWrapper>
     </PageWrapper>
   );
