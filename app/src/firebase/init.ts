0658d57db283fa9c8fe322524a7954ad463c1956
Samantha Barli <samanthasalley@gmail.com>
2022-11-15 19:03:18 -0500

    connect app to firebase / firestore

---
+++ app/src/firebase/init.ts
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { getApp, initializeApp } from 'firebase/app';
+
+// retrieve config data stored in ENV
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+};
+
+// make sure we only initialize once
+const initializeAppIfNecessary = () => {
+  try {
+    return getApp();
+  } catch {
+    return initializeApp(firebaseConfig);
+  }
+};
+
+// Initialize Firebase
+export const firebase = initializeAppIfNecessary();
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import './firebase/init';
 import { App } from './components/app';
 import * as serviceWorker from './serviceWorker';
 import { store } from './store';
